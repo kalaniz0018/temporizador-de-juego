5da229a9a9fa5b770a51208cc9472ed0edf77ac8
kalaniz0018 <karinaalaniz1994@gmail.com>
2024-04-11 11:46:07 -0300

    reemplazando el onChange con el useRef

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,30 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 const Player = () => {
-  // enteredPlayerName para almacenar el nombre del jugador ingresado
-  const [enteredPlayerName, setEnteredPlayerName] = useState('');
-  //submitted para rastrear si se ha enviado el nombre del jugador.
-  const [submitted, setSubmitted] = useState(false);
+  const playerName = useRef();
 
-  // handleChange se activa cada vez que el usuario escribe en el campo de entrada de texto. Actualiza el estado 
-  //enteredPlayerName con el valor actual del campo de entrada.
-  function handleChange(event) {
-    setSubmitted(false)
-    setEnteredPlayerName(event.target.value);
-  }
-//handleClick es una función que se activa cuando el usuario hace clic en el botón "Set Name". 
-//Establece el estado submitted en true
-  function handleClick(){
-    setSubmitted(true)
-  }
+  const [enteredPlayerName, setEnteredPlayerName] = useState(null);
 
+  function handleClick() {
+    setEnteredPlayerName(playerName.current.value);
+  }
 
   return (
     <section id="player">
-      <h2>Welcome {submitted ? enteredPlayerName : 'unknown entity'}</h2>
+      <h2>Welcome {enteredPlayerName ?? "unknown entity"}</h2>
+      <h2>
+        Welcome {enteredPlayerName ? enteredPlayerName : "unknown entity"}
+      </h2>
       <p>
-        <input type="text" onChange={handleChange} value={enteredPlayerName} />
-        <button  onClick={handleClick}>Set Name</button>
+        <input type="text" ref={playerName} />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
