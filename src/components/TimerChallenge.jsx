ad26fa5d4de53c34d793f08d8a85cff9b176c87e
kalaniz0018 <karinaalaniz1994@gmail.com>
2024-04-15 12:26:48 -0300

    usando  forwardRef para utilizar el ref entre dos componentes

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -5,10 +5,11 @@ const TimerChallenge = ({ title, targetTime }) => {
   const [timerExpired, setTimerExpired] = useState(false);
   const [timerStarted, setTimerStarted] = useState(false);
   const timer = useRef();
-
+  const dialog = useRef();
   function handleStart() {
     timer.current = setTimeout(() => {
       setTimerExpired(true);
+      dialog.current.showModal();
     }, targetTime * 1000);
     setTimerStarted(true);
   }
@@ -19,10 +20,11 @@ const TimerChallenge = ({ title, targetTime }) => {
 
   return (
     <>
-      {timerExpired && <ResultModal targetTime={targetTime} result="Lost" />}
+      <ResultModal ref={dialog} targetTime={targetTime} result="Lost" />
+
       <section className="challenge">
         <h2>{title}</h2>
-        
+
         <p className="challenge-time">
           {targetTime} second{targetTime > 1 ? "s" : ""}
         </p>
